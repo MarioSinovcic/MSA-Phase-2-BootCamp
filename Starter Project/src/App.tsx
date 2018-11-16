9cb4283e5359b5b63c29f22cac0ab43a84baab1d
Mario <mrmariosinovcic@gmail.com>
2018-11-17 11:19:16 +1300

    Added Functionality
    Search functionaity added for tags

--- Starter Project/src/App.tsx
+++ Starter Project/src/App.tsx
@@ -23,6 +23,9 @@ class App extends React.Component<{}, IState> {
 			uploadFileList: null
 		}     	
 		this.selectNewMeme = this.selectNewMeme.bind(this)
+
+		this.fetchMemes = this.fetchMemes.bind(this)
+		this.fetchMemes("")	
 	}
 
 	public render() {
@@ -41,7 +44,7 @@ class App extends React.Component<{}, IState> {
 						<MemeDetail currentMeme={this.state.currentMeme} />
 					</div>
 					<div className="col-5">
-						<MemeList memes={this.state.memes} selectNewMeme={this.selectNewMeme} searchByTag={this.methodNotImplemented}/>
+						<MemeList memes={this.state.memes} selectNewMeme={this.selectNewMeme} searchByTag={this.fetchMemes}/>
 					</div>
 				</div>
 			</div>
@@ -88,6 +91,28 @@ class App extends React.Component<{}, IState> {
 		this.setState({
 			currentMeme: newMeme
 		})
+	}
+
+	// Added via MSA Repo
+	private fetchMemes(tag: any) {
+		let url = "http://phase2apitest.azurewebsites.net/api/meme"
+		if (tag !== "") {
+			url += "/tag?=" + tag
+		}
+		fetch(url, {
+			method: 'GET'
+		})
+		.then(res => res.json())
+		.then(json => {
+			let currentMeme = json[0]
+			if (currentMeme === undefined) {
+				currentMeme = {"id":0, "title":"No memes (╯°□°）╯︵ ┻━┻","url":"","tags":"try a different tag","uploaded":"","width":"0","height":"0"}
+			}
+			this.setState({
+				currentMeme,
+				memes: json
+			})
+		});
 	}
 }
 
