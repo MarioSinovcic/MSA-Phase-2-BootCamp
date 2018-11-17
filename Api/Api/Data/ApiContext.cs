da0ae79ff561c5343c2e38ce81d209e6b7908b82
Mario <mrmariosinovcic@gmail.com>
2018-11-17 15:16:32 +1300

    Creating an Api
    upto step 7

---
+++ Api/Api/Data/ApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Api.Models;
+
+namespace Api.Models
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext (DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Api.Models.MemeItem> MemeItem { get; set; }
+    }
+}
